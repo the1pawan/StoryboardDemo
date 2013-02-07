a92b80914aed59caa8bd86607deba0db480f86fb
Pawankumar Dhamane <pawankumardhamane@gmail.com>
2013-02-07 14:22:25 +0530

    Storyboard button and its action to go to next screen

---
+++ StoryboardButtonAction2/StoryboardDemo/Tab1Button1ViewController.m
@@ -0,0 +1,38 @@
+//
+//  Tab1Button1ViewController.m
+//  StoryboardDemo
+//
+//  Created by Pawan Dhamane on 07/02/13.
+//  Copyright (c) 2013 the1pawan. All rights reserved.
+//
+
+#import "Tab1Button1ViewController.h"
+
+@interface Tab1Button1ViewController ()
+
+@end
+
+@implementation Tab1Button1ViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
---
+++ StoryboardNavigationTableView1/StoryboardDemo/Tab1ViewController.m
@@ -0,0 +1,29 @@
+//
+//  detailViewController.m
+//  StoryboardDemo
+//
+//  Created by Pawan Dhamane on 07/02/13.
+//  Copyright (c) 2013 the1pawan. All rights reserved.
+//
+
+#import "Tab1ViewController.h"
+
+@interface Tab1ViewController ()
+
+@end
+
+@implementation Tab1ViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
