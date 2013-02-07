c8c9ab765fe6a56eb59d969a4bb8bee4916d4cd0
Pawankumar Dhamane <pawankumardhamane@gmail.com>
2013-02-07 12:37:40 +0530

    Basic storyboard with Tabbar Controller & Navigation Controller

---
+++ StoryboardDemo/Tab1ViewController.h
@@ -0,0 +1,13 @@
+//
+//  detailViewController.h
+//  StoryboardDemo
+//
+//  Created by Pawan Dhamane on 07/02/13.
+//  Copyright (c) 2013 the1pawan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface Tab1ViewController : UIViewController
+
+@end
---
+++ StoryboardDemo/detailAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  detailAppDelegate.h
+//  StoryboardDemo
+//
+//  Created by Pawan Dhamane on 07/02/13.
+//  Copyright (c) 2013 the1pawan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface detailAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
